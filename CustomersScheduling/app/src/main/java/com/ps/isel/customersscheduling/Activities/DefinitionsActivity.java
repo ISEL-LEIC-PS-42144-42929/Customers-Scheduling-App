b487080371b5526dd1d9b045740c01dd9e9a19dd
Colapso <goncalomariabarros@gmail.com>
2018-04-04 20:02:32 +0100

    GeneralDefinitions Activity almost done, problems in back arrow Listener

--- CustomersScheduling/app/src/main/java/com/ps/isel/customersscheduling/Activities/DefinitionsActivity.java
+++ CustomersScheduling/app/src/main/java/com/ps/isel/customersscheduling/Activities/DefinitionsActivity.java
@@ -5,11 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
 
 import com.ps.isel.customersscheduling.R;
+import com.ps.isel.customersscheduling.Utis.CustomAdapterButtons;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DefinitionsActivity extends AppCompatActivity
 {
+    private ArrayList<String> list = new ArrayList<String>(Arrays.asList("111,222,333,444,555,666".split(",")));
+
+    private String[] subbedBusiness = new String[]
+            {
+                    "Edit profile",
+                    "Change e-mail",
+                    "Change password",
+                    "Notifications",
+            };
+
+    private ListView lv;
+
     private Toolbar toolbar;
 
     @Override
@@ -17,6 +34,9 @@ public class DefinitionsActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definitions);
+
+        lv = (ListView) findViewById(R.id.listButtons);
+        lv.setAdapter(new CustomAdapterButtons(subbedBusiness, this) );
 
         toolbar = (Toolbar) findViewById(R.id.filter_toolbar);
         setSupportActionBar(toolbar);
@@ -27,7 +47,7 @@ public class DefinitionsActivity extends AppCompatActivity
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
---
+++ CustomersScheduling/app/src/main/java/com/ps/isel/customersscheduling/Utis/CustomAdapterButtons.java
@@ -0,0 +1,71 @@
+package com.ps.isel.customersscheduling.Utis;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.ps.isel.customersscheduling.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Colapso on 04/04/18.
+ */
+
+public class CustomAdapterButtons extends BaseAdapter
+{
+    private String[] buttonsName;
+    private Context context;
+
+    public CustomAdapterButtons(String[] buttonsName, Context context)
+    {
+        this.buttonsName = buttonsName;
+        this.context = context;
+    }
+
+
+    @Override
+    public int getCount()
+    {
+        return buttonsName.length;
+    }
+
+    @Override
+    public Object getItem(int i)
+    {
+        return buttonsName[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent)
+    {
+        View view = convertView;
+
+        if (view == null)
+        {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.custom_adapter_buttons_layout, null);
+        }
+
+        Button defName= (Button)view.findViewById(R.id.btn);
+        defName.setText(buttonsName[position]);
+
+        defName.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //do something
+
+            }
+        });
+
+        return view;
+    }
+}
