c48bf2c4667f50934d41da7b11cbd14496b583c0
Colapso <goncalomariabarros@gmail.com>
2018-04-06 19:44:30 +0100

    Main Favourites Activity done

--- CustomersScheduling/app/src/main/java/com/ps/isel/customersscheduling/Activities/FavouritesActivity.java
+++ CustomersScheduling/app/src/main/java/com/ps/isel/customersscheduling/Activities/FavouritesActivity.java
@@ -1,16 +1,35 @@
 package com.ps.isel.customersscheduling.Activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.ps.isel.customersscheduling.Activities.BaseLayoutsActivities.BaseActivityWithOnlyBackArrow;
 import com.ps.isel.customersscheduling.R;
 
-public class FavouritesActivity extends AppCompatActivity
+public class FavouritesActivity extends BaseActivityWithOnlyBackArrow
 {
+    ArrayAdapter<String> adapter;
+
+    private String[] favSearches = new String[]
+            {
+                    "Barbeiros em Lisboa",
+                    "Pediatras em Benfica",
+                    "Restaurantes em Cascais",
+                    "Spa em Lisboa",
+            };
+
+
+    private ListView lv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favourites);
+        getSupportActionBar().setTitle("Favourites");
+       lv = (ListView) findViewById(R.id.listButtons);
+       adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,favSearches);
+       lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, favSearches) );
+
     }
 }
